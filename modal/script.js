47bb58ac507f03313a6f16615106ca015e77c9d7
shuvo18103107 <18103107@iubat.edu>
2021-06-27 15:03:47 +0600

    📝 refactor the code

--- modal/script.js
+++ modal/script.js
@@ -5,28 +5,24 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btncloseModal = document.querySelector('.close-modal');
 const btnsOpneModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpneModal);
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+const openModal = function () {
+    modal.classList.remove('hidden');
+
+    overlay.classList.remove('hidden');
+
+}
+
 for (let i = 0; i < btnsOpneModal.length; i++)
     btnsOpneModal[i].addEventListener(
-        'click', function () {
-            console.log(btnsOpneModal[i].textContent);
-            modal.classList.remove('hidden');
-
-            overlay.classList.remove('hidden');
-
-
-
-
-
-        }
+        'click', openModal
     );
-btncloseModal.addEventListener('click', function () {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
 
-})
-overlay.addEventListener('click', function () {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
 
-})
+
+btncloseModal.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal)
