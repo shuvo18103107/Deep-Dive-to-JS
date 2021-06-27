3be4c662e48495604dceb495a2e9f4eb29d2b8f4
shuvo18103107 <18103107@iubat.edu>
2021-06-27 14:54:48 +0600

    🚀 modal done

--- modal/script.js
+++ modal/script.js
@@ -7,4 +7,26 @@ const btncloseModal = document.querySelector('.close-modal');
 const btnsOpneModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpneModal);
 for (let i = 0; i < btnsOpneModal.length; i++)
-    console.log(btnsOpneModal[i].textContent);
+    btnsOpneModal[i].addEventListener(
+        'click', function () {
+            console.log(btnsOpneModal[i].textContent);
+            modal.classList.remove('hidden');
+
+            overlay.classList.remove('hidden');
+
+
+
+
+
+        }
+    );
+btncloseModal.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+
+})
+overlay.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+
+})
