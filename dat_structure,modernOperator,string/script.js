fe4346fc2bb11ed23d2b0315280dc85eccd22174
shuvo18103107 <18103107@iubat.edu>
2021-07-18 20:28:04 +0600

    📝 destructing arrays

---
+++ dat_structure,modernOperator,string/script.js
@@ -0,0 +1,66 @@
+'use strict';
+// destructing arrays
+// break complex data structure into simple one and store it in a variable
+// // Data needed for a later exercise
+// const flights =
+//     '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// Data needed for first part of the section
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    order: function (startterIndex, mainIndex) {
+        return [this.starterMenu[startterIndex], this.mainMenu[mainIndex]]
+    },
+
+    openingHours: {
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0, // Open 24 hours
+            close: 24,
+        },
+    },
+};
+
+const arr = [2, 3, 4];
+// destructing the array
+const [x, y, z] = arr;
+console.log(x, y, z);
+const [first, second] = restaurant.categories;
+console.log(first, second);
+const [firste, , , thirde] = restaurant.categories;
+console.log(firste, thirde);
+let [main, , secondary] = restaurant.categories
+console.log(main, secondary);
+//switch the dishes
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+[main, secondary] = [secondary, main]
+console.log(main, secondary);
+
+// method theke calculative result ta destruct korbo 
+const [starter, maindish] = restaurant.order(2, 0)
+console.log(starter, maindish);
+const nested = [2, 4, [5, 6]];
+
+// const [i, , j] = nested;
+// console.log(i, j);
+
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+// default values
+const [p = 1, q = 1, r = 1] = [8];
+
+console.log(p, q, r);
