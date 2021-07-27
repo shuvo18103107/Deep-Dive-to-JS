876dd42c202b689c0a0924316ffa5dd30422b1aa
shuvo18103107 <18103107@iubat.edu>
2021-07-27 14:11:04 +0600

    ✅  third codin challenge

---
+++ dat_structure,modernOperator,string/which ds should choose.js
@@ -0,0 +1,18 @@
+// which data structure should choose
+// there are 3 source of data 
+//1. data in programe(source code), 2. from the ui 3. from web api
+
+// 4 types of ds in js set,object,map,array
+// if there is a simple list of value then we use set or array
+// if it is key value pair then map or object
+// in list type of data simply we have just value but in key value pair , in key we access the value
+// geeting data from a web api , data from webapi are json formatted
+//json is normally a long string but it can be converted into js object
+// when to use what ds 
+// for array : simple list of values when we do not need to describe values , or manipulate the data 
+// set: simple list but unique value, use to remove duplicatetes from arrays, set is give high performance than array
+
+// when we need to describe value in key value structure use object or maps
+//object: traditional key value structure, and it has some technical disadvantages, this keyword is useful, use when u need to include function(methods)
+//maps is better for key value store introduce in es6, also map keys can be any data type
+//use when working with json(can be converted into map)
