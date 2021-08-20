a1fe762f5c0bcbc2d1bbab885507414cb5db1caf
shuvo18103107 <18103107@iubat.edu>
2021-08-20 21:07:51 +0600

    ✅ coding challenge on protyping

---
+++ oop/coding challenge1.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const Car = function (make, speed) {
+    this.make = make;
+    this.speed = speed;
+
+}
+
+Car.prototype.accerate = function () {
+    console.log(this.speed = this.speed + 10);
+
+}
+Car.prototype.break = function () {
+    console.log(this.speed = this.speed - 5);
+
+}
+
+const carobj1 = new Car('BMW', 120)
+const carobj2 = new Car('Mercedes', 95)
+console.log(carobj1);
+console.log(carobj2);
+carobj1.accerate();
+carobj1.accerate();
+carobj1.break();
+carobj2.accerate();
+carobj2.break();
