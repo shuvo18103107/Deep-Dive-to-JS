7a37dc920947a68d2c57ff1ebe2cc2e2b2434530
shuvo18103107 <18103107@iubat.edu>
2021-09-02 20:16:55 +0600

    ✅ module pattern

--- Modules and tooling/script.js
+++ Modules and tooling/script.js
@@ -1,4 +1,4 @@
-//importing modules
+/*//importing modules
 // import './shoppingCart.js';
 // import { addTocart, qt, shippingCart, totalPrice as price } from './shoppingCart.js';
 // console.log(shippingCart);
@@ -19,6 +19,43 @@ console.log('importing modules');
 //import default export
 import add, { totalPrice, qt, shippingCart } from './shoppingCart.js'
 console.log(totalPrice, qt, shippingCart);
-
+*/
 //we ca n mix together deafult export and name export but better avoid that style
 //*note: imports are not copy of exports its a live connection of export
+//Module pattern es6
+//we use ife , so we don't need to call it manually and also it execute one time
+const shoppingCart2 = (function (params) {
+    //all of the variable and method are private in the function scop
+    const cart = [];
+    const shippingcost = 20;
+    const totalPrice = 237;
+    const totalQuantity = 23;
+    const addTocart = function (product, quantity) {
+        cart.push(product, quantity);
+        console.log(`${quantity} ${product} added to cart`);
+    };
+
+    const orderStock = function (product, quantity) {
+        console.log(`${quantity} ${product} order from supplier`);
+    };
+    return {
+        //it contains which stuffs we want to public
+        addTocart,
+        cart,
+        totalPrice,
+
+        totalQuantity,
+    };
+})();
+shoppingCart2.addTocart('apples', 4)
+console.log(shoppingCart2.shippingcost);//cg this is privat 
+
+//common js module
+//it will not work in browser but work in nodejs
+//export
+// export.addToCart =function (product, quantity) {
+//     cart.push(product, quantity);
+//     console.log(`${quantity} ${product} added to cart`);
+// };
+// //import
+// import { addTocart } = require('./shoppingCart');
