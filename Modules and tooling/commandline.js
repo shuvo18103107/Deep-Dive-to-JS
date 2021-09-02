7a5452a3614d1c1ed21d13025eccef0497a136ae
shuvo18103107 <18103107@iubat.edu>
2021-09-02 23:16:40 +0600

    🔥 node packages

---
+++ Modules and tooling/commandline.js
@@ -0,0 +1,19 @@
+//ls/dir -> to list up all files
+
+//cd .. ->to go down a file also
+//cd ../.. ->to move down twol level
+//to go to a file - cd '.\adb driver\'
+//clear the console -> clear
+// mkdir foldername -> to create a folder - like mkdir test
+//make file on  a folder -> edit index.html or edit script.js
+
+//delete a file-> del filename
+
+
+/*
+npm installing
+ check version -> npm -v
+ initializing -> npm init
+ then it will ask some question to name the package.json file
+
+*/
--- Modules and tooling/script.js
+++ Modules and tooling/script.js
@@ -24,31 +24,31 @@ console.log(totalPrice, qt, shippingCart);
 //*note: imports are not copy of exports its a live connection of export
 //Module pattern es6
 //we use ife , so we don't need to call it manually and also it execute one time
-const shoppingCart2 = (function (params) {
-    //all of the variable and method are private in the function scop
-    const cart = [];
-    const shippingcost = 20;
-    const totalPrice = 237;
-    const totalQuantity = 23;
-    const addTocart = function (product, quantity) {
-        cart.push(product, quantity);
-        console.log(`${quantity} ${product} added to cart`);
-    };
+// const shoppingCart2 = (function () {
+//     //all of the variable and method are private in the function scop
+//     const cart = [];
+//     const shippingcost = 20;
+//     const totalPrice = 237;
+//     const totalQuantity = 23;
+//     const addTocart = function (product, quantity) {
+//         cart.push(product, quantity);
+//         console.log(`${quantity} ${product} added to cart`);
+//     };
 
-    const orderStock = function (product, quantity) {
-        console.log(`${quantity} ${product} order from supplier`);
-    };
-    return {
-        //it contains which stuffs we want to public
-        addTocart,
-        cart,
-        totalPrice,
+//     const orderStock = function (product, quantity) {
+//         console.log(`${quantity} ${product} order from supplier`);
+//     };
+//     return {
+//         //it contains which stuffs we want to public
+//         addTocart,
+//         cart,
+//         totalPrice,
 
-        totalQuantity,
-    };
-})();
-shoppingCart2.addTocart('apples', 4)
-console.log(shoppingCart2.shippingcost);//cg this is privat 
+//         totalQuantity,
+//     };
+// })();
+// shoppingCart2.addTocart('apples', 4);
+// console.log(shoppingCart2.shippingcost); //cg this is privat
 
 //common js module
 //it will not work in browser but work in nodejs
@@ -59,3 +59,29 @@ console.log(shoppingCart2.shippingcost);//cg this is privat
 // };
 // //import
 // import { addTocart } = require('./shoppingCart');
+
+import { clone } from 'lodash';
+import { cloneDeep } from './node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+    cart: [
+        { product: 'bread', quantity: 5 },
+        {
+            product: 'piza',
+            quantity: 41,
+        },
+    ],
+
+    user: {
+        logged: false
+    }
+};
+
+//copy this object in traditional way
+const statcopy = Object.assign({}, state)
+console.log(statcopy);
+statcopy.user.logged = true;
+console.log(statcopy); //copied object will also updated
+//but now i want to deep clone the object
+statdeepClone = cloneDeep(state)
+console.log(statdeepClone);
